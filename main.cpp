06e077bab0b2aaa66525ea26a9655176bf91edeb
Chris Sanrow <58667107+chrissanrow@users.noreply.github.com>
2025-06-21 03:54:35 -0700

    Added sphere ray intersecting logic

--- main.cpp
+++ main.cpp
@@ -5,11 +5,26 @@
 #include <iostream>
 using namespace std;
 
+bool hit_sphere(const point3 &center, double radius, const ray &r)
+{
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray &r)
 {
+    // hard coded sphere for now
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    {
+        return color(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0); // abs value of y
-    return (1.0 - a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+    auto a = 0.5 * (unit_direction.y() + 1.0); // abs value of y
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main()
